5c2627a38558b5d51f17fbe7ff8c2f5cf234394b
TuanLQHe171554 <tuanlqhe171554@fpt.edu.vn>
2025-03-10 21:51:50 +0700

    Update App.js

--- App/App.js
+++ App/App.js
@@ -1,23 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './src/redux/store';
+import { LoginScreen } from './src/screens/Auth/Login';
+import { SignUpScreen } from './src/screens/Auth/SignUp';
 import BottomNavigation from './src/screens/BottomNavigation';
+import { ProductDetail } from './src/screens/HomePage/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 
-
+let persistor = persistStore(store);
 
 export default function App() {
   return (
-    // <Provider store={store}>
-    //   <PersistGate persistor={persistor}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={BottomNavigation} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    //   </PersistGate>
-    // </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name="Home" component={BottomNavigation} options={{ headerShown: false }} />
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="ProductDetail" component={ProductDetail} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
 
 
   );
